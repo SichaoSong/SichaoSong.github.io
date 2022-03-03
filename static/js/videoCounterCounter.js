98047ba88e320ea966ac2a64357025a86a968c3c
SichaoSong <song_sichao@cyberagent.co.jp>
2022-03-03 12:50:37 +0900

    update counter

--- static/js/videoCounterCounter.js
+++ static/js/videoCounterCounter.js
@@ -3,15 +3,16 @@ const formCounter = document.getElementById('counter_form')
 const btnCounterSave = document.getElementById('counter_save_btn')
 const btnCounterDelete = document.getElementById('counter_delete_btn')
 
+let timeHistory = [null, null, null] // [timeStart, timeEnd, timeDuration]
 const counterList = {}
 const counterBaseTemplate = {
   'sum':
     '<li style="padding: 0px;">' +
     '  <div class="uk-button uk-button-default uk-inline counter-container">' +
-    '    <div class="uk-text-small uk-text-bold counter-name" style="overflow: scroll; height: 20px;"></div>' +
+    '    <div class="uk-text-small uk-text-bold counter-name" style="overflow: scroll; height: 40px;"></div>' +
     '    <div class="uk-label uk-label-default uk-position-bottom-left no-padding counter-type"></div>' +
     '    <div class="uk-position-center">' +
-    '      <div class="uk-text uk-text-large counter-sum">0</div>' +
+    '      <div class="uk-text uk-text-large counter-sum">null</div>' +
     '    </div>' +
     '    <a href="#" class="uk-icon-link uk-position-top-right" uk-icon="pencil" onclick="editCounter(this);event.stopPropagation();"></a>' +
     '  </div>' +
@@ -19,27 +20,40 @@ const counterBaseTemplate = {
   'time':
     '<li style="padding: 0px;">' +
     '  <div class="uk-button uk-button-default uk-inline counter-container">' +
-    '    <div class="uk-text-small uk-text-bold counter-name" style="overflow: scroll; height: 20px;"></div>' +
+    '    <div class="uk-text-small uk-text-bold counter-name" style="overflow: scroll; height: 40px;"></div>' +
     '    <div class="uk-label uk-label-warning uk-position-bottom-left no-padding counter-type"></div>' +
     '    <div class="uk-position-center">' +
-    '      <div class="uk-text uk-text-small uk-text-muted counter-time-start">0</div>' +
-    '      <div class="uk-text uk-text-small uk-text-muted counter-time-end">0</div>' +
+    '      <div class="uk-text uk-text-small uk-text-muted counter-time-start">null</div>' +
+    '      <div class="uk-text uk-text-small uk-text-muted counter-time-end">null</div>' +
     '      <div class="uk-text uk-text-large uk-text-emphasis uk-text-primary counter-time-duration">0</div>' +
     '    </div>' +
     '    <a href="#" class="uk-icon-link uk-position-top-right" uk-icon="pencil" onclick="editCounter(this);event.stopPropagation();"></a>' +
     '  </div>' +
     '</li>',
-  'label':
-    '<li style="padding: 0px;">' +
-    '  <div class="uk-button uk-button-default uk-inline counter-container">' +
-    '    <div class="uk-text-small uk-text-bold counter-name"  style="overflow: scroll; height: 20px;"></div>' +
-    '    <div class="uk-label uk-label-danger uk-position-bottom-left no-padding counter-type"></div>' +
-    '    <div class="uk-position-center">' +
-    '      <div class="uk-text uk-text-large counter-label">-</div>' +
-    '    </div>' +
-    '    <a href="#" class="uk-icon-link uk-position-top-right" uk-icon="pencil" onclick="editCounter(this);event.stopPropagation();"></a>' +
-    '  </div>' +
-    '</li>',
+  // 'label':
+  //   '<li style="padding: 0px;">' +
+  //   '  <div class="uk-button uk-button-default uk-inline counter-container">' +
+  //   '    <div class="uk-text-small uk-text-bold counter-name"  style="overflow: scroll; height: 20px;"></div>' +
+  //   '    <div class="uk-label uk-label-danger uk-position-bottom-left no-padding counter-type"></div>' +
+  //   '    <div class="uk-position-center">' +
+  //   '      <div class="uk-text uk-text-large counter-label">-</div>' +
+  //   '    </div>' +
+  //   '    <a href="#" class="uk-icon-link uk-position-top-right" uk-icon="pencil" onclick="editCounter(this);event.stopPropagation();"></a>' +
+  //   '  </div>' +
+  //   '</li>',
+}
+
+const keyCharMap = {
+  Escape: 'ESC',
+  Control: 'CTL',
+  Shift: 'SFT',
+  Backspace: 'BS',
+  Enter: 'ENT',
+  ArrowLeft: '←',
+  ArrowUp: '↑',
+  ArrowDown: '↓',
+  ArrowRight: '→',
+  ' ': 'SPC'
 }
 
 function createDOM (htmlString) {
@@ -63,16 +77,12 @@ function addCounter () {
       color: '#FFFFFF'
     }
     counter.name = formCounter.querySelector('#name_input').value ? formCounter.querySelector('#name_input').value : counter.name
-    counter.type = formCounter.querySelector('#type_sum_input').checked ? 'sum' :
-      formCounter.querySelector('#type_time_input').checked ? 'time' :
-        'label'
+    counter.type = formCounter.querySelector('#type_sum_input').checked ? 'sum' : 'time'
     counter.key = key ? key : counter.key
     counter.color = formCounter.querySelector('#color_input').value
     counterList[counter.name] = counter
     let child = null
-    counter.type === 'sum' ? child = createDOM(counterBaseTemplate['sum']) :
-      counter.type === 'time' ? child = createDOM(counterBaseTemplate['time']) :
-        child = createDOM(counterBaseTemplate['label'])
+    counter.type === 'sum' ? child = createDOM(counterBaseTemplate['sum']) : child = createDOM(counterBaseTemplate['time'])
     child.querySelector('.counter-container').id = counter.name
     child.querySelector('.counter-container').setAttribute('key', counter.key)
     child.querySelector('.counter-name').innerText = counter.name
@@ -91,7 +101,7 @@ function editCounter (dom) {
   UIkit.modal('#counter_form').show()
   const counterOldName = dom.parentNode.id
   const counter = counterList[counterOldName]
-  let key = ''
+  let key = counter.key
   formCounter.querySelector('#key_input').onkeyup = (e) => {
     key = keyCharMap[e.key] ? keyCharMap[e.key] : e.key.toUpperCase()
     formCounter.querySelector('#key_input').value = key
@@ -99,8 +109,7 @@ function editCounter (dom) {
   // update counter setting display before edit
   formCounter.querySelector('#name_input').value = counter.name
   counter.type === 'sum' ? formCounter.querySelector('#type_sum_input').checked = true :
-      counter.type === 'time' ? formCounter.querySelector('#type_time_input').checked = true  :
-        formCounter.querySelector('#type_label_input').checked = true
+    formCounter.querySelector('#type_time_input').checked = true
   formCounter.querySelector('#key_input').value = counter.key
   formCounter.querySelector('#color_input').value = counter.color
   btnCounterSave.onclick = ()=> {
@@ -131,11 +140,65 @@ function editCounter (dom) {
   }
 }
 
+function createCounter (name, key, type, color, value) {
+  const counter = {
+    name: name,
+    type: type,
+    key: key,
+    color: color
+  }
+  counterList[counter.name] = counter
+  let child = null
+  counter.type === 'sum' ? child = createDOM(counterBaseTemplate['sum']) : child = createDOM(counterBaseTemplate['time'])
+  child.querySelector('.counter-container').id = counter.name
+  child.querySelector('.counter-container').setAttribute('key', counter.key)
+  child.querySelector('.counter-name').innerText = counter.name
+  child.querySelector('.counter-type').innerText = counter.type
+  child.querySelector('.counter-container').onclick = () => {
+    manageCounter(child, counter)
+  }
+  child.querySelector('.counter-container').style.backgroundColor = counter.color
+  const children = [...counterArea.childNodes].filter(item => item.tagName === 'LI')
+  counterArea.insertBefore(child, children[children - 1])
+  if (counter.type === 'sum') {
+    counter.sum = parseInt(value)
+    child.querySelector('.counter-sum').innerText = counter.sum
+  } else if (counter.type === 'time') {
+    counter.timeDuration = parseFloat(value)
+    child.querySelector('.counter-time-duration').innerText = counter.timeDuration.toFixed(2)
+  }
+  counterList[counter.name] = counter
+  updateCounterList(counter)
+}
+
+function createCounters (data) {
+  if (data.length > 0 && data[0].length > 0) {
+    const names = data[0]
+    const keys = data[1]
+    const types = data[2]
+    const colors = data[3]
+    const values = data[data.length - 1]
+    for (let i = 1; i < names.length; i++) {
+      const name = names[i]
+      const key = keys[i]
+      const type = types[i]
+      const color = colors[i]
+      const value = values[i]
+      createCounter(name, key, type, color, isNewVideo ? null : value)
+    }
+  }
+}
+
 function manageCounter (dom, counter) {
   switch (counter.type) {
     case 'sum': {
       if (counter.sum) {
-        counter.sum += 1
+        if (shiftFlag) {
+          counter.sum -= 1
+          shiftFlag = !shiftFlag
+        } else {
+          counter.sum += 1
+        }
       } else {
         counter.sum = 1
       }
@@ -145,43 +208,133 @@ function manageCounter (dom, counter) {
     }
 
     case 'time': {
-      if (!counter.timeStart) {
-        counter.timeStart = video.currentTime
-        dom.querySelector('.counter-time-start').innerText = counter.timeStart.toFixed(2)
-        dom.querySelector('.counter-time-end').innerText = '-'
-      } else {
-        counter.timeEnd = video.currentTime
-        // calculate sum of time duration
-        if (!counter.timeDuration) {
-          counter.timeDuration = counter.timeEnd - counter.timeStart
+      if (shiftFlag) {
+        if (!counter.timeStart) {
+          if (counter.timeStart) {
+            counter.timeStart = timeHistory[0]
+            dom.querySelector('.counter-time-start').innerText = counter.timeStart.toFixed(2)
+            dom.querySelector('.counter-time-end').innerText = '-'
+            counter.timeDuration -= (timeHistory[1] - timeHistory[0])
+            dom.querySelector('.counter-time-duration').innerText = counter.timeDuration.toFixed(2)
+          }
         } else {
-          counter.timeDuration += counter.timeEnd - counter.timeStart
+          counter.timeStart = null
+          dom.querySelector('.counter-time-start').innerText = '-'
+          dom.querySelector('.counter-time-end').innerText = '-'
         }
-        dom.querySelector('.counter-time-end').innerText = counter.timeEnd.toFixed(2)
-        dom.querySelector('.counter-time-duration').innerText = counter.timeDuration.toFixed(2)
-        counter.timeStart = null
-        counter.timeEnd = null
+        shiftFlag = !shiftFlag
+        timeHistory = [null, null, null]
+      } else {
+        if (!counter.timeStart) {
+          counter.timeStart = video.currentTime
+          dom.querySelector('.counter-time-start').innerText = counter.timeStart.toFixed(2)
+          dom.querySelector('.counter-time-end').innerText = '-'
+        } else {
+          counter.timeEnd = video.currentTime
+          // calculate sum of time duration
+          if (!counter.timeDuration) {
+            counter.timeDuration = counter.timeEnd - counter.timeStart
+          } else {
+            counter.timeDuration += counter.timeEnd - counter.timeStart
+          }
+          dom.querySelector('.counter-time-end').innerText = counter.timeEnd.toFixed(2)
+          dom.querySelector('.counter-time-duration').innerText = counter.timeDuration.toFixed(2)
+          timeHistory = [counter.timeStart, counter.timeEnd, counter.duration]
+          counter.timeStart = null
+          counter.timeEnd = null
+        }
       }
       updateCounterList(counter)
       break
     }
 
-    case 'label': {
-      if (!counter.label || counter.label === 'no') {
-        counter.label = 'yes'
-        dom.querySelector('.counter-label').innerText = counter.label
-      } else {
-        counter.label = 'no'
-        dom.querySelector('.counter-label').innerText = counter.label
-      }
-      updateCounterList(counter)
-      break
-    }
+    // case 'label': {
+    //   if (!counter.label || counter.label === 'no') {
+    //     counter.label = 'yes'
+    //     dom.querySelector('.counter-label').innerText = counter.label
+    //   } else {
+    //     counter.label = 'no'
+    //     dom.querySelector('.counter-label').innerText = counter.label
+    //   }
+    //   updateCounterList(counter)
+    //   break
+    // }
   }
 }
 
 function updateCounterList (counter) {
   Object.assign(counter)
+}
+
+function updateResults () {
+  let temp = []
+  // append readFile to results
+  if (Object.keys(counterList).length === 0) return
+  if (readFile.length > 0) {
+    for (let i = 0; i < readFile.length; i++) {
+      results.push(readFile[i])
+    }
+    readFile = []
+    newResult = false
+  } else {
+    if (newResult) {
+      temp = ['']
+      // first row
+      for (const [name, counter] of Object.entries(counterList)) {
+        temp.push(name)
+      }
+      results.push(temp)
+      temp = ['']
+      for (const [name, counter] of Object.entries(counterList)) {
+        temp.push(counter.key)
+      }
+      results.push(temp)
+      temp = ['']
+      for (const [name, counter] of Object.entries(counterList)) {
+        temp.push(counter.type)
+      }
+      results.push(temp)
+      temp = ['']
+      for (const [name, counter] of Object.entries(counterList)) {
+        temp.push(counter.color)
+      }
+      results.push(temp)
+      temp = []
+    }
+    // append annotation results
+    // if it is a same video, remove the old results and add new results
+    if (!isNewVideo) {
+      results.pop()
+    }
+    temp.push(videoName)
+    for (const [name, counter] of Object.entries(counterList)) {
+      // console.log(name)
+      switch (counter.type) {
+        case 'sum':
+          temp.push(counter.sum)
+          break
+
+        case 'time':
+          temp.push(counter.timeDuration)
+          break
+      }
+    }
+    results.push(temp)
+    newResult = false
+    console.log(results)
+  }
+}
+
+function initCounters () {
+  if (Object.keys(counterList).length === 0) return
+  clearCounters()
+  createCounters(results)
+}
+
+function clearCounters () {
+  while (counterArea.firstElementChild.nextElementSibling) {
+    counterArea.removeChild(counterArea.firstElementChild.nextElementSibling)
+  }
 }
 
 document.onkeyup = (e) => {
@@ -206,6 +359,10 @@ document.onkeyup = (e) => {
 
       case '←':
         btnBackward10s.click()
+        break
+
+      case keyCharMap.Shift:
+        shiftFlag = !shiftFlag
         break
 
       default: {
--- static/js/videoCounterMain.js
+++ static/js/videoCounterMain.js
@@ -1,4 +1,10 @@
+// eslint settings
+/* UIkit */
+
+/* インタフェース関連 */
 const open = document.getElementById('open')
+const read = document.getElementById('read')
+const save = document.getElementById('save')
 const video = document.getElementById('video').querySelector('video')
 const btnPlay = document.getElementById('btn_play')
 const btnForward = document.getElementById('btn_forward')
@@ -7,22 +13,15 @@ const btnBackward = document.getElementById('btn_backward')
 const btnBackwardBadge = btnBackward.querySelector('.uk-badge')
 const btnForward10s = document.getElementById('btn_forward10s')
 const btnBackward10s = document.getElementById('btn_backward10s')
-
+/* 内部制御関連 */
 let forwardRate = 1
 let backwardRate = 1
-
-const keyCharMap = {
-  Escape: 'ESC',
-  Control: 'CTL',
-  Shift: 'SFT',
-  Backspace: 'BS',
-  Enter: 'ENT',
-  ArrowLeft: '←',
-  ArrowUp: '↑',
-  ArrowDown: '↓',
-  ArrowRight: '→',
-  ' ': 'SPC'
-}
+let videoName = ''
+let isNewVideo = true
+let shiftFlag = false
+let readFile = []
+let results = []
+let newResult = true
 
 open.addEventListener('change', (e) => {
   init()
@@ -31,6 +30,37 @@ open.addEventListener('change', (e) => {
   video.src = url.createObjectURL(file)
   video.hidden = false
   video.parentNode.querySelector('i').hidden = true
+  // check out if it is a new video or not
+  isNewVideoOrNot(videoName, file)
+  // update video name
+  videoName = file.name
+})
+
+read.addEventListener('change', (e) => {
+  if (videoName === '') {
+    UIkit.notification({
+      message: '<span uk-icon=\'icon: close\'></span><span class="notif">先に動画を開いてください</span>',
+      status: 'danger',
+      timeout: 4000,
+      pos: 'top-right'
+    })
+    read.value = ''
+    return
+  }
+  const file = e.target.files[0]
+  const reader = new FileReader()
+  reader.readAsText(file)
+  reader.onload = (event) => {
+    const result = event.target.result
+    makeCSV(result, file)
+  }
+})
+
+save.addEventListener('click', () => {
+  updateResults()
+  if (results.length > 0) {
+    downloadCSV(results.map(e => e.toString()).join('\n'), videoName.slice(0, videoName.length - 4) + '.csv')
+  }
 })
 
 btnPlay.addEventListener('click', (e) => {
@@ -105,6 +135,26 @@ btnBackward10s.addEventListener('click', () => {
   }
 })
 
+video.addEventListener('playing', () => {
+  const icon = btnPlay.querySelector('i')
+  if (icon.classList.contains('fa-play-circle')) {
+    icon.classList.remove('fa-play-circle')
+  }
+  if (!icon.classList.contains('fa-pause-circle')) {
+    icon.classList.add('fa-pause-circle')
+  }
+})
+
+video.addEventListener('pause', () => {
+  const icon = btnPlay.querySelector('i')
+  if (icon.classList.contains('fa-pause-circle')) {
+    icon.classList.remove('fa-pause-circle')
+  }
+  if (!icon.classList.contains('fa-play-circle')) {
+    icon.classList.add('fa-play-circle')
+  }
+})
+
 function init () {
   const btnPlayIcon = btnPlay.querySelector('i')
   if (btnPlayIcon.classList.contains('fa-pause-circle')) {
@@ -115,6 +165,10 @@ function init () {
   btnForwardBadge.innerText = 1
   backwardRate = 1
   btnForwardBadge.innerText = 1
+  // update results
+  updateResults()
+  // initialize counters
+  initCounters()
 }
 
 function play (e) {
@@ -134,3 +188,63 @@ function changeSpeed (e, speed) {
     e.playbackRate = speed
   }
 }
+
+function makeCSV (result, file) {
+  const tmp = result.split("\n").filter(e => e !== '')
+  let data = []
+  for (let i = 0; i < tmp.length; i++) {
+    const rowDate = tmp[i]
+    data[i] = rowDate.split(",")
+  }
+  readFile = data
+  console.log(readFile)
+  // check out if it is a new video or not
+  isNewVideoOrNot(file.name, file)
+  // make buttons
+  createCounters(readFile)
+  // update results
+  updateResults()
+}
+
+function downloadCSV(data, filename) {
+  //BOMを付与する（Excelでの文字化け対策）
+  const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+  //Blobでデータを作成する
+  const blob = new Blob([bom, data], { type: "text/csv" });
+
+  //IE10/11用(download属性が機能しないためmsSaveBlobを使用）
+  if (window.navigator.msSaveBlob) {
+    window.navigator.msSaveBlob(blob, filename);
+    //その他ブラウザ
+  } else {
+    //BlobからオブジェクトURLを作成する
+    const url = (window.URL || window.webkitURL).createObjectURL(blob);
+    //ダウンロード用にリンクを作成する
+    const download = document.createElement("a");
+    //リンク先に上記で生成したURLを指定する
+    download.href = url;
+    //download属性にファイル名を指定する
+    download.download = filename;
+    //作成したリンクをクリックしてダウンロードを実行する
+    download.click();
+    //createObjectURLで作成したオブジェクトURLを開放する
+    (window.URL || window.webkitURL).revokeObjectURL(url);
+  }
+}
+
+function isNewVideoOrNot (videoname, file) {
+  if (readFile.length > 0) {
+    const latestvideoname = readFile[readFile.length - 1][0]
+    isNewVideo = videoname === latestvideoname ? false : true
+  } else {
+    isNewVideo = videoname === file.name ? false : true
+  }
+  console.log(isNewVideo)
+}
+
+window.addEventListener('beforeunload', (event) => {
+  updateResults()
+  if (results.length > 0) {
+    downloadCSV(results.map(e => e.toString()).join('\n'), 'backup_' + videoName.slice(0, videoName.length - 4) + '.csv')
+  }
+})
